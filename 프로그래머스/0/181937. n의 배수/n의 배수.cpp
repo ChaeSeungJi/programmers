3e935bebc848370efb66c46c54392cc69749c9e3
채승지 <69447192+ChaeSeungJi@users.noreply.github.com>
2025-03-15 16:54:37 +0900

    [level 0] Title: n의 배수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num, int n) {
+    int answer = 0;
+    int result = num % n;
+    if(result == 0)
+        answer = 1;
+    else
+        answer = 0;
+    return answer;
+}
