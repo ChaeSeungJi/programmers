ec735d6c3e82273d926cedf04f85b307ad35a175
채승지 <69447192+ChaeSeungJi@users.noreply.github.com>
2025-03-25 22:05:52 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_suffix) {
+    int answer = 0;
+    
+    for(int i =0; i<my_string.size(); i++){
+        if(is_suffix == my_string.substr(i)){
+            answer = 1;
+            break;
+        }
+    }
+    
+    return answer;
+}
