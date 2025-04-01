5bc8bcd67218b0f01e5739dbc7b33517bd34a145
채승지 <69447192+ChaeSeungJi@users.noreply.github.com>
2025-04-01 15:55:53 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(char& a : myString){
+        if(a == 97)
+            a -= 32;
+        else if( a>= 66 && a<=90)
+            a +=32;
+    }
+    answer = myString;
+    return answer;
+}
