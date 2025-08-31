a70dd691de666238c6d2ad19b229666cd9174aae
채승지 <69447192+ChaeSeungJi@users.noreply.github.com>
2025-08-31 23:14:38 +0900

    [level 2] Title: 올바른 괄호, Time: 0.45 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,26 @@
+#include<string>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> st;
+    for(char c : s){
+        if(c == '('){
+            st.push(c);
+        }
+        else{
+            if(st.empty()){
+                return false;
+            }
+            else{
+                st.pop();
+            }
+        }
+    }
+    if(!st.empty()) return false;
+    return answer;
+
+}
